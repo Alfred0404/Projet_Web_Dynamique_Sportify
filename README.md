2b334304a6a9f0ee357b1637457fd7c76585e6e7
Alfred de Vulpian <derfladv@gmail.com>
2024-05-27 20:43:09 +0200

    🆕 Ajout : - création de la structure du site

---
+++ README.md
@@ -0,0 +1,18 @@
+# Projet de Web du second semestre d'ING2
+
+## Sportify
+
+Ce site permet de mettre en relation des clients et des coachs sportifs en tout genre, pour une multitude d'activitées sportives.
+
+## Stack
+
+- HTML
+- PHP
+- CSS
+- JS
+- JQuery
+- Bootstrap
+
+Les wireframes ont été réalisées grâce a Figma
+
+Le SGBD utilisé est phpmyadmin.
---
+++ src/compte.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Votre compte</title>
+</head>
+
+<body>
+    <header>
+
+    </header>
+
+    </div>
+    <div class="nav">
+        <ul>
+            <li><a href="accueil.php">Accueil</a></li>
+            <li><a href="parcourir.php">Tout parcourir</a></li>
+            <li><a href="recherche.php">Rechercher</a></li>
+            <li><a href="rendez_vous.php">Rendez-vous</a></li>
+            <li><a href="#">Votre compte</a></li>
+        </ul>
+    </div>
+    <section>
+
+    </section>
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
